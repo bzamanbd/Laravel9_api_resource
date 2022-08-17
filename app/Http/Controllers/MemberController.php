041fd7649d0456f97ebd48305c3a6b50f5cd7ca3
Md. Bodiuzzaman Ripon <riponzamanbd@gmail.com>
2022-08-17 17:02:20 +0600

    first commit

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Member;
+use Illuminate\Support\Facades\Validator;
+
+
+class MemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Member::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $rules = array(
+            'name'=>'required',
+            'address'=>'required',
+        );
+        $validator = Validator::make($request->all(),$rules);
+        if (!$validator->fails()) {
+            $member = new Member;
+            $member->name = $request->name;
+            $member->address = $request->address;
+            $result=$member->save();
+            if ($result) {
+                return 'one data added';
+            } else {
+
+                return response()->json($validator->errors(),401);
+            }
+        } else {
+            return response()->json($validator->errors(),401);
+
+        }
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
